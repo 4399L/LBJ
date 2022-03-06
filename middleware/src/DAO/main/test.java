6b2f314f66a95c0111654e1e55d6ff4db1339588
4399L <4399L@1301286925@qq.com>
2022-03-06 17:24:08 +0800

    commit middleware file

---
+++ middleware/src/DAO/main/test.java
@@ -0,0 +1,46 @@
+package DAO.main;
+import DAO.bean.Student;
+import DAO.dao.StudentDAO;
+import DAO.daoIMP.StudentDAOIMP;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import DAO.connection.DataBaseConnection;
+
+public class test {
+    public static void main(String[] args) {
+        StudentDAO dao = new StudentDAOIMP();
+        //1.findAll()
+        ArrayList<Student> stu1 = dao.findAll();
+        for(Student s:stu1) {
+            System.out.println(s);}
+
+        //2.findByID()
+        //System.out.println(dao.findByID(1910300127));
+
+        //3.insert()
+//        Student s3 = new Student();
+//        s3.setID(1910300133);
+//        s3.setName("小王");
+//        dao.insert(s3);
+//        ArrayList<Student> stu2 = dao.findAll();
+//        for (Student s : stu2) {
+//            System.out.println(s);
+//        }
+
+        //4.delete()
+//        dao.delete(1910300127);
+//        ArrayList<Student> stu3 = dao.findAll();
+//        for(Student s:stu3) {
+//            System.out.println(s);}
+
+        //5.update()
+//        s3.setID(1910300130);
+//        s3.setName("小李");
+//        dao.update(s3);
+//        ArrayList<Student> stu4 = dao.findAll();
+//        for (Student s : stu4) {
+//            System.out.println(s);
+//        }
+    }
+}
